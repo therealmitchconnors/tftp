92ad4279ac20625bd7f13459feaedd82ad69bef1
Mitch Connors <mitchconnors@gmail.com>
2018-08-25 21:38:01 -0700

    Set up basic serving

--- cmd/tftpd/main.go
+++ cmd/tftpd/main.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello world!")
-
-	// TODO implement the in-memory tftp server
+	// TODO: get port number from cmd line
+	port := 2000
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Handle the request in another thread, allowing
+		// the main thread to keep accepting new connections.
+		go func(c net.Conn) {
+			c.Close()
+		}(conn)
+	}
 }
